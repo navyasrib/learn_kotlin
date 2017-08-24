7351ae5f46bca2f47935b5590d992f0f3c2ab05a
NavyaSriB <basava@thoughtworks.com>
2017-08-24 21:33:46 +0530

    Factorial with Big integer

--- src/paly_ground.kt
+++ src/paly_ground.kt
@@ -1,19 +1,17 @@
+import java.math.BigInteger
+
 fun main(args: Array<String>) {
     val data = listOf(1, 2, 3, 4, 5)
     println(perform(data, ::addValues))
     println(perform(data, ::multiply))
-    println(factorial(20))
+    println(factorial(BigInteger("8391")))
 }
 
-fun factorial(limit: Int): Int = if (limit == 1) 1 else limit * factorial(limit - 1)
+fun perform(data: List<Int>, method: (Int, Int) -> Int) = data.reduce{ a, b -> method(a, b) }
 
+fun addValues(a: Int, b: Int) = a.and(b)
 
-fun perform(data: List<Int>, method: (Int, Int) -> Int): Int = data.reduce({ a, b -> method(a, b) })
+fun multiply(a: Int, b: Int) = a.times(b)
 
-fun addValues(a: Int, b: Int): Int = a + b
-
-fun multiply(a: Int, b: Int): Int = a * b
-
-
-
-
+val one = BigInteger("1")
+fun factorial(limit: BigInteger, result: BigInteger = one): BigInteger = if (limit <= one) result else factorial(limit.minus(one), limit.times(result))
