bfbf3c59dd91a5e0d6cfd5173016f259e9a7fa29
NavyaSriB <basava@thoughtworks.com>
2017-08-28 10:56:43 +0530

    pythogoreanTriplets and tailrec for factorial

--- src/paly_ground.kt
+++ src/paly_ground.kt
@@ -4,14 +4,27 @@ fun main(args: Array<String>) {
     val data = listOf(1, 2, 3, 4, 5)
     println(perform(data, ::addValues))
     println(perform(data, ::multiply))
-    println(factorial(BigInteger("8391")))
+    println(factorial(BigInteger("500")))
+    println(pythagoreanTriples(50))
 }
 
-fun perform(data: List<Int>, method: (Int, Int) -> Int) = data.reduce{ a, b -> method(a, b) }
+fun perform(data: List<Int>, method: (Int, Int) -> Int) = data.reduce { a, b -> method(a, b) }
 
 fun addValues(a: Int, b: Int) = a.and(b)
 
 fun multiply(a: Int, b: Int) = a.times(b)
 
 val one = BigInteger("1")
-fun factorial(limit: BigInteger, result: BigInteger = one): BigInteger = if (limit <= one) result else factorial(limit.minus(one), limit.times(result))
+tailrec fun factorial(limit: BigInteger, result: BigInteger = one): BigInteger =
+        if (limit <= one)
+            result
+        else
+            factorial(limit.minus(one), limit.times(result))
+
+fun pythagoreanTriples(limit: Int) = (1..limit).flatMap { a ->
+    (a..limit).flatMap { b ->
+        (b..limit).filter { c ->
+            a.times(a) + b.times(b) == c.times(c)
+        }.map { Triple(a, b, it) }
+    }
+}
